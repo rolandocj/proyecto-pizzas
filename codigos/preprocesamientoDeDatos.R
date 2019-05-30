db9f48bdbbc58cd538f8361d7e2fe34c59dd6658
ricardo27cruz27 <ricardo.cruz@cimat.mx>
2019-05-29 19:07:23 -0500

    Se edito el archivo de lectura de datos. se lee directo desde github

--- codigos/preprocesamientoDeDatos.R
+++ codigos/preprocesamientoDeDatos.R
@@ -1,1 +1,16 @@
 #### PREPROCESAMIENTO DE DATOS ####
+
+#Se varga libreria para carga de datos desde github
+library(RCurl)
+
+#Carga de la base de datos
+url<-"https://raw.githubusercontent.com/rolandocj/proyecto-pizzas/develop/datos/pizzas.csv"
+x<-getURL(url)
+pizzas<-read.csv(text=x)
+
+
+#Nombres de las columnas
+#names(pizzas)
+names(pizzas)<-(c("Id","Humedad","Proteina","Grasa","Ceniza","Sodio","Carbohidratos","Calorias","Marca"))
+#Fijamos los nombres como variables
+attach(pizzas)
